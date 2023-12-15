07baa1dad18c3acc5e003cb7b7c58e37c53594c6
kathy1124 <11056018@ntub.edu.tw>
2023-12-16 00:12:14 +0800

    models

--- mytext/models.py
+++ mytext/models.py
@@ -12,3 +12,12 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.text
+    
+

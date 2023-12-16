5a4162a49fed360cb484b3a750964730bbb438b4
kathy1124 <11056018@ntub.edu.tw>
2023-12-16 12:20:46 +0800

    comment

--- mytext/admin.py
+++ mytext/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from mytext.models import Post
+from mytext.models import Post, Comment
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'pub_date')
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
+
+
